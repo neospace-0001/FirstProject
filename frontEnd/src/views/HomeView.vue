23382e50c1370dd289e15d9fe2247653d113c26b
billClinton048 <clintonkaranja46@gmail.com>
2025-03-12 09:36:22 +0300

    Added the Herosection

--- frontEnd/src/views/HomeView.vue
+++ frontEnd/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
+import TopNav from '@/components/TopNav.vue';
 
 </script>
 
 <template>
-  <main>
-    <h1 class="text-red-500 text-xl" >Hello There</h1>
-  </main>
+ <section>
+  <TopNav />
+ </section>
 </template>
